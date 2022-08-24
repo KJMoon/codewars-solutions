77f7dce73b27be2cb341e7108871ce43314b87a8
Eric Moon <moonkangjin9195@gmail.com>
2022-08-23 23:04:25 -0400

    Adding daily coding challenge. Day 6

---
+++ 8kyu/is-string-upcase.js
@@ -0,0 +1,20 @@
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+// My solution
+
+String.prototype.isUpperCase = function(){
+    return String(this) === this.toUpperCase()
+}
+
+// Other solution
+// String.prototype.isUpperCase = function(){
+//     return this === this.toUpperCase()
+// }
