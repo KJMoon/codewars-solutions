88d7674d15939289eda76d2a793aaf78240047f8
moonkangjin <moonkangjin9195@gmail.com>
2022-08-25 23:54:31 -0400

    Adding daily coding challenge. Day 8

--- 8kyu/count-positive-sum-negative.js
+++ 8kyu/count-positive-sum-negative.js
@@ -7,6 +7,7 @@
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
+//My solution
 function countPositivesSumNegatives(input){
     if(input == null || input.length == 0){
       return []
