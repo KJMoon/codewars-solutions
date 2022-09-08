6249ef5d2f2584be026e7f6da2e9fda0102b5f9d
Eric Moon <moonkangjin9195@gmail.com>
2022-09-07 22:34:08 -0400

    Adding daily coding challenge.

---
+++ 8kyu/are-you-playing-banjo.js
@@ -0,0 +1,34 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as it's only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+// My solution
+
+function areYouPlayingBanjo(name) {
+    if(name[0] === "R" || name[0] === "r"){
+      return name + " plays banjo"
+    }else{
+      return name + " does not play banjo"
+    }
+}
+
+// Other solutions
+
+// function areYouPlayingBanjo(name) {
+//     return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+// }
+
+// OR
+
+// function areYouPlayingBanjo(name) {
+//     if (name[0].toLowerCase() === 'r') {
+//       return name + ' plays banjo';
+//     } else {
+//       return name + ' does not play banjo';
+//     }
+// }
