0b752c1d6ed49e3f93c4813475d353aa775a99bb
Eric Moon <moonkangjin9195@gmail.com>
2022-09-12 22:17:15 -0400

    Adding daily coding challenge.

---
+++ 8kyu/corr-mistakes-of-software.js
@@ -0,0 +1,31 @@
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+// My solution
+
+function correct(string){
+	let splitStr = string.split('')
+    
+  let correctMistakes = splitStr.map(char => {
+    if(char === '5'){
+      return 'S'
+    }else if(char === '0'){
+      return 'O'
+    }else if(char === '1'){
+      return 'I'
+    }else{
+      return char
+    }
+  })
+  
+  return correctMistakes.join('')
+}
+
