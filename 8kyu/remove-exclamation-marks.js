5a304e3811e46166a2667a5d072b7a5f612b9830
Eric Moon <moonkangjin9195@gmail.com>
2022-10-28 18:26:29 -0400

    Adding daily coding challenge.

---
+++ 8kyu/remove-exclamation-marks.js
@@ -0,0 +1,13 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// My solution
+
+function removeExclamationMarks(s) {
+    return s.split('').filter(char => char != '!').join('');
+};
+
+// Another solution
+
+// function removeExclamationMarks(s) {
+//     return s.split('!').join('');
+// }
